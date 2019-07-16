2ca72ba6cfc04b4dc6ff2a31470e4b532a9a2154
Antti Karvanen <antti.karvanen@exove.fi>
2019-07-16 11:37:29 +0300

    4.2

---
+++ osa4/blogilista/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const app = express()
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,22 +1,7 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const mongoose = require('mongoose')
-
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
+const app = require('./app')
 
 
-//const mongoUrl = 'mongodb+srv://fsopen-user:fsopen@fsopen-va6ql.mongodb.net/bloglist-app?retryWrites=true'
-mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true })
-
-app.use(cors())
-app.use(bodyParser.json())
-
-app.use('/api/blogs', blogsRouter)
-//const PORT = 3003
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
 })
