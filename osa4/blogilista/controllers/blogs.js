6dbba002eaef4fbd7f61438254862f0e8d49d2c0
Antti Karvanen <antti.karvanen@exove.fi>
2019-07-16 11:25:21 +0300

    controller and config utils

---
+++ osa4/blogilista/controllers/blogs.js
@@ -0,0 +1,34 @@
+const blogsRouter = require('express').Router()
+const mongoose = require('mongoose')
+//TODO Blog to model
+//const Blog = require('../models/blog')
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+
+blogsRouter.get('/', (request, response) => {
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+})
+
+blogsRouter.post('/', (request, response) => {
+  const blog = new Blog(request.body)
+
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+
+module.exports = blogsRouter
